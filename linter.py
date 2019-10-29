aef650b81fabff4cb01c3136f3752d240b4222be
Arjen <arjenzijlstra@users.noreply.github.com>
2019-10-29 15:34:48 +0100

    Remove unnecessary braces

--- linter.py
+++ linter.py
@@ -5,7 +5,7 @@ class SaltLint(lint.Linter):
     name = 'salt-lint'
     cmd = ('salt-lint', '--nocolor', '${args}', '${file}')
     regex = (r'(?P<code>\d+) (?P<message>.+)\s+'
-            r'(.+):(?P<line>\d+)\s+'
+            r'.+:(?P<line>\d+)\s+'
             r'(?P<near>.+)')
     multiline = True
     default_type = lint.WARNING
