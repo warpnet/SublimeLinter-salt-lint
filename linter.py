702984d51908529769df04c435d78660081254f9
Roald Nefs <roald@warpnet.nl>
2019-10-31 20:55:34 +0100

    Fix under-indent in linter.py
    Fix under-indent in `linter.py`.
    
    Signed-off-by: Roald Nefs <roald@warpnet.nl>

--- linter.py
+++ linter.py
@@ -5,8 +5,8 @@ class SaltLint(lint.Linter):
     name = 'salt-lint'
     cmd = ('salt-lint', '--nocolor', '${args}', '${file}')
     regex = (r'(?P<code>\d+) (?P<message>.+)\s+'
-            r'.+:(?P<line>\d+)\s+'
-            r'(?P<near>.+)')
+             r'.+:(?P<line>\d+)\s+'
+             r'(?P<near>.+)')
     multiline = True
     default_type = lint.WARNING
     defaults = {
