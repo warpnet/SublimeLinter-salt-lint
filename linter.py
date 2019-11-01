f9cd9af2def8eecb523d4e709eafefea99efcb89
Arjen <arjenzijlstra@users.noreply.github.com>
2019-11-01 10:29:03 +0100

    Update the linter.py to accomodate for change in the next release of salt-lint

--- linter.py
+++ linter.py
@@ -3,8 +3,8 @@ from SublimeLinter import lint
 
 class SaltLint(lint.Linter):
     name = 'salt-lint'
-    cmd = ('salt-lint', '--nocolor', '${args}', '${file}')
-    regex = (r'(?P<code>\d+) (?P<message>.+)\s+'
+    cmd = ('salt-lint', '--nocolor', '${args}')
+    regex = (r'\[(?P<code>\d+)\] (?P<message>.+)\s+'
              r'.+:(?P<line>\d+)\s+'
              r'(?P<near>.+)')
     multiline = True
